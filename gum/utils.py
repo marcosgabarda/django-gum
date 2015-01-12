99a4e760e380f06ffb5ef8b2dcffba8e2618d819
Marcos Gabarda <marcosgabarda@gmail.com>
2015-01-12 16:19:12 +0100

    cached connections

--- gum/utils.py
+++ gum/utils.py
@@ -6,12 +6,24 @@ from elasticsearch import Elasticsearch
 from gum.settings import ELASTICSEARCH_URLS, ELASTICSEARCH_TIMEOUT
 
 
+def _build_key(urls):
+    key = (urls,)
+    return key
+
+
+_cached_elasticsearch = {}
+
+
 def elasticsearch_connection(urls=None):
     """Create an elasticsearch `Elasticsearch` object and return it."""
     connection_urls = urls or ELASTICSEARCH_URLS
+    key = _build_key(urls)
+    if key in _cached_elasticsearch:
+        return _cached_elasticsearch[key]
     try:
         _ = iter(connection_urls)
     except TypeError:
         connection_urls = [connection_urls]
     es = Elasticsearch(hosts=connection_urls, timeout=ELASTICSEARCH_TIMEOUT)
+    _cached_elasticsearch[key] = es
     return es
