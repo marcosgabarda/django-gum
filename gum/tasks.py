196d9ad7ea6938f81e9db0f8b31ece58ab0eafe4
Marcos Gabarda <marcosgabarda@gmail.com>
2015-01-12 10:50:48 +0100

    celery support

--- gum/tasks.py
+++ gum/tasks.py
@@ -3,12 +3,12 @@ from __future__ import unicode_literals
 from celery.task import task
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ObjectDoesNotExist
-from gum.indexer import indexer, NotRegistered
 
 
 @task
 def handle_save(sender_content_type_pk, instance_pk):
     """Async task to handle the indexation of a model."""
+    from gum.indexer import indexer, NotRegistered
     try:
         sender_content_type = ContentType.objects.get(pk=sender_content_type_pk)
         sender = sender_content_type.model_class()
@@ -26,6 +26,7 @@ def handle_save(sender_content_type_pk, instance_pk):
 @task
 def handle_delete(sender_content_type_pk, instance_pk):
     """Async task to delete a model from the index."""
+    from gum.indexer import indexer, NotRegistered
     try:
         sender_content_type = ContentType.objects.get(pk=sender_content_type_pk)
         sender = sender_content_type.model_class()
