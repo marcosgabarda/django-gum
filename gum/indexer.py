f3a86011b74e91db8cec5fb74f3dd0cbc16d3151
Marcos Gabarda <marcosgabarda@gmail.com>
2015-01-15 11:09:45 +0100

    show progress in command line

--- gum/indexer.py
+++ gum/indexer.py
@@ -172,8 +172,8 @@ class Indexer(object):
             instances = model.objects.all()
             total_instances = instances.count()
             if stdout:
-                stdout.write("Indexing %s instances from %s " % str(total_instances, model))
-                stdout.write("Indexing %s instances from %s " % str(total_instances, model))
+                stdout.write("Indexing %s instances from %s " % (total_instances, str(model)))
+                stdout.write("Indexing %s instances from %s " % (total_instances, str(model)))
             mapping_type.create_mapping_type()
             for step, instance in enumerate(instances):
                 mapping_type.index_document(instance)
