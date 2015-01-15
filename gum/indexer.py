29bfdc9c4255dec62b405b710ad1d3f568e0f9f5
Marcos Gabarda <marcosgabarda@gmail.com>
2015-01-15 11:13:49 +0100

    show progress in command line

--- gum/indexer.py
+++ gum/indexer.py
@@ -177,7 +177,7 @@ class Indexer(object):
             for step, instance in enumerate(instances):
                 mapping_type.index_document(instance)
                 if stdout:
-                    progress = ((step + 1) / float(total_instances)) * 100
+                    progress = int(((step + 1) / float(total_instances)) * 100)
                     stdout.write('\r')
                     stdout.write("[%-100s] %d%%" % ('='*progress, progress))
                     stdout.flush()
