9b68d320022f79777d9e534731c2b58180e8e433
Marcos Gabarda <marcosgabarda@gmail.com>
2015-01-15 11:37:49 +0100

    dynamic width

--- gum/indexer.py
+++ gum/indexer.py
@@ -180,7 +180,7 @@ class Indexer(object):
                     import os
                     progress = int((step + 1) / float(total_instances))
                     _, columns = os.popen('stty size', 'r').read().split()
-                    graph_progress = progress * min(columns, 100)
+                    graph_progress = progress * min(columns - 10, 100)
                     stdout.write('\r', ending='')
                     progress_format = "[%-{}s] %d%%".format(min(columns, 100))
                     stdout.write(progress_format % ('='*graph_progress, progress*100), ending='')
