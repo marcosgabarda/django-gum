298455bb49dc130f08abafd71a43fc63e4b41b2a
Marcos Gabarda <marcosgabarda@gmail.com>
2015-06-17 09:54:44 +0200

    select models to index

--- gum/management/commands/gum_update_index.py
+++ gum/management/commands/gum_update_index.py
@@ -2,15 +2,29 @@
 from __future__ import unicode_literals
 
 from django.core.management.base import BaseCommand
+from django.db.models.loading import get_model
 from gum.indexer import indexer
 
 
 class Command(BaseCommand):
 
+    args = "<app_name.model_name app_name.model_name ...>"
     help = "Creates and updates index"
 
     def handle(self, *args, **options):
+
+        # Parse arguments
+        selected_models = []
+        for input_model in args:
+            try:
+                app_name, model_name = input_model.split(".")
+                model_class = get_model(app_name, model_name)
+                selected_models.append(model_class)
+            except (ValueError, LookupError):
+                pass
+        restrict_to = selected_models if selected_models else None
+
         self.stdout.write('Initializing index...', ending='')
         indexer.initialize_index()
         self.stdout.write(' OK')
-        indexer.update_index(stdout=self.stdout)
+        indexer.update_index(stdout=self.stdout, restrict_to=restrict_to)
