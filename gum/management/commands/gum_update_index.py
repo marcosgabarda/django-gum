1acb8d7c758ffbc0fe74222410429eaaced1d965
Marcos Gabarda <marcosgabarda@gmail.com>
2015-08-28 18:04:59 +0200

    using new application loading system for get_model

--- gum/management/commands/gum_update_index.py
+++ gum/management/commands/gum_update_index.py
@@ -2,8 +2,14 @@
 from __future__ import unicode_literals
 
 from django.core.management.base import BaseCommand
-from django.db.models.loading import get_model
+
 from gum.indexer import indexer
+
+try:
+    from django.apps import apps
+    get_model = apps.get_model
+except ImportError:
+    from django.db.models.loading import get_model
 
 
 class Command(BaseCommand):
